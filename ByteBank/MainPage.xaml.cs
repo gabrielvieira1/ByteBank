688331b0f90c2f1cbb8041169a3f5145f21af6a2
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-07-08 20:06:13 -0300

    Project created

---
+++ ByteBank/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ByteBank
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    private AppServiceConnection deviceService;
+
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    private async void btnClick_Click(object sender, RoutedEventArgs e)
+    {
+      // Add the connection.
+      if (this.deviceService == null)
+      {
+        this.deviceService = new AppServiceConnection();
+
+        // Here, we use the app service name defined in the app service 
+        // provider's Package.appxmanifest file in the <Extension> section.
+        this.deviceService.AppServiceName = "com.microsoft.deviceInfo";
+
+        // Use Windows.ApplicationModel.Package.Current.Id.FamilyName 
+        // within the app service provider to get this value.
+        this.deviceService.PackageFamilyName = "1e0301ff-f819-4eb6-a97f-8bda5de6c1cb_03w1eqkrn0fpt"; // Package.Current.Id.FamilyName;
+
+        var status = await this.deviceService.OpenAsync();
+
+        if (status != AppServiceConnectionStatus.Success)
+        {
+          textBox.Text = "Failed to connect";
+          this.deviceService = null;
+          return;
+        }
+      }
+
+      // Call the service.
+      int idx = int.Parse(textBox.Text);
+      var message = new ValueSet();
+      message.Add("Command", "Item");
+      message.Add("ID", idx);
+      AppServiceResponse response = await this.deviceService.SendMessageAsync(message);
+      string result = "";
+
+      if (response.Status == AppServiceResponseStatus.Success)
+      {
+        ////Get the data that the service sent to us.
+        if (response.Message["Status"] as string == "OK")
+        {
+          result = response.Message["Result"] as string;
+        }
+        await Launcher.LaunchUriAsync(new Uri("calculator://"));
+      }
+
+      textBox.Text = result;
+    }
+  }
+}
---
+++ MyAppService/DeviceUserName.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+
+namespace MyAppService
+{
+  public sealed class DeviceUserName : IBackgroundTask
+  {
+    private BackgroundTaskDeferral backgroundTaskDeferral;
+    private AppServiceConnection appServiceconnection;
+
+    [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+    private static extern int MessageBoxW(
+      IntPtr hWnd,
+      [param: MarshalAs(UnmanagedType.LPWStr)] string lpText,
+      [param: MarshalAs(UnmanagedType.LPWStr)] string lpCaption,
+      UInt32 utype);
+
+
+    [DllImport("Advapi32.dll", CharSet = CharSet.Unicode)]
+    private static extern bool GetUserNameW(
+      [param: MarshalAs(UnmanagedType.LPWStr)] StringBuilder lpBuffer,
+      ref UInt32 pcbBuffer);
+
+    public void Run(IBackgroundTaskInstance taskInstance)
+    {
+      // Get a deferral so that the service isn't terminated.
+      this.backgroundTaskDeferral = taskInstance.GetDeferral();
+
+      // Associate a cancellation handler with the background task.
+      taskInstance.Canceled += OnTaskCanceled;
+
+      // Retrieve the app service connection and set up a listener for incoming app service requests.
+      var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+      appServiceconnection = details.AppServiceConnection;
+      appServiceconnection.RequestReceived += OnRequestReceived;
+    }
+
+    private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+    {
+      var messageDeferral = args.GetDeferral();
+
+      ValueSet message = args.Request.Message;
+      ValueSet returnData = new ValueSet();
+
+      //int result = MessageBoxW(IntPtr.Zero, "Hello World", "This is window title", 0);
+
+      StringBuilder sb = new StringBuilder(100);
+      UInt32 size = 100;
+
+      GetUserNameW(sb, ref size);
+
+      returnData.Add("Result", sb.ToString());
+      returnData.Add("Status", "OK");
+
+      // Return the data to the caller.
+      await args.Request.SendResponseAsync(returnData);
+
+      // Complete the deferral so that the platform knows that we're done responding to the app service call.
+      // Note for error handling: this must be called even if SendResponseAsync() throws an exception.
+      messageDeferral.Complete();
+    }
+
+
+    private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+    {
+      if (this.backgroundTaskDeferral != null)
+      {
+        // Complete the service deferral.
+        this.backgroundTaskDeferral.Complete();
+      }
+    }
+  }
+}
